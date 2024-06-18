7647cc52089043d3f597abac27222099bc07f27b
Carlos Abraão Dordenoni Holanda <carlos.holanda@prosperi.com.br>
2024-06-18 10:54:46 -0300

    section 5

---
+++ CursoCsharp/section_5/MainProdutoDois.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CursoCsharp.section_4;
+
+namespace CursoCsharp.section_5
+{
+    internal class MainProdutoDois
+    {
+        static void Main(string[] args)
+        {
+            ProdutoDois p = new ProdutoDois("TV", 500.00, 10);
+
+            Console.WriteLine(p.Nome);
+
+            p.Nome = "TV 4K";
+            Console.WriteLine(p.Nome);
+            Console.WriteLine(p.Preco);
+            Console.WriteLine(p.Quantidade);
+        }
+    }
+}
---
+++ CursoCsharp/section_5/ProdutoDois.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCsharp.section_5
+{
+    internal class ProdutoDois
+    {
+        
+        // Ordem recomendada:
+        private String _nome;
+        public double Preco { get; private set; }
+        public int Quantidade { get; private set; }
+
+        public ProdutoDois(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public ProdutoDois(string nome, double preco)
+        {
+            _nome = nome;
+            Preco = preco;
+        }
+
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Quantidade * Preco;
+        }
+
+        public void AdicionarProdutos(int qtd)
+        {
+            Quantidade += qtd;
+        }
+
+        public void RemoverProdutos(int qtd)
+        {
+            Quantidade -= qtd;
+        }
+
+        public override string ToString()
+        {
+            return _nome + ", $ " + Preco.ToString("F2") + ", Unidades: " + Quantidade + ", Valor total: $ " + ValorTotalEmEstoque().ToString("F2");
+        }
+    }
+}
