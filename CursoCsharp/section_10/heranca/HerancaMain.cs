09aa09d6a5b5bac78bdc2bbbdec14204e495c025
Carlos Abraão <carlosabraao@hotmail.com.br>
2024-06-20 19:53:24 -0300

    section 10 - part 1

---
+++ CursoCsharp/section_10/heranca/HerancaMain.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCsharp.section_10.heranca
+{
+    internal class HerancaMain
+    {
+        static void Main(string[] args)
+        {
+            Account acc = new Account(1001, "Alex", 0);
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0, 500);
+
+            // UPCASTING
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(1003, "Bob", 0, 200);
+
+            // DOWNCASTING
+            BusinessAccount acc3 = (BusinessAccount)acc2;
+            acc3.Loan(100);
+        }
+    }
+}
