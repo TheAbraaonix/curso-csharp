85182cd49c845b619e8b869f490345d8dacde6c5
Carlos Abraão <carlosabraao@hotmail.com.br>
2024-06-25 17:42:32 -0300

    section 15 - part 2

---
+++ CursoCsharp/section_15/DictionaryAndSortedDictionary/DictionaryMain.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCsharp.section_15.DictionaryAndSortedDictionary
+{
+    internal class DictionaryMain
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+            cookies["User"] = "Maria";
+            cookies["email"] = "maria@gmail.com";
+            cookies["phone"] = "998124241";
+            cookies["phone"] = "53626246";
+
+            Console.WriteLine(cookies["email"]);
+            Console.WriteLine(cookies["phone"]);
+
+            cookies.Remove("email");
+            
+            if (cookies.ContainsKey("email"))
+            {
+                Console.WriteLine(cookies["email"]);
+            }
+            else
+            {
+                Console.WriteLine("There is no email key");
+            }
+
+            Console.WriteLine("Size: " + cookies.Count());
+
+            Console.WriteLine("All Cookies:");
+            foreach (KeyValuePair<string, string> item in cookies)
+            {
+                Console.WriteLine(item.Key + ", " + item.Value);
+            }
+        }
+    }
+}
