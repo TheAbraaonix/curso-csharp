fb7a4a7383fc92648636991d6ccc2b16a54e71cf
Carlos Abraão <carlosabraao@hotmail.com.br>
2024-06-26 19:09:14 -0300

    section 17 - part 2

---
+++ CursoCsharp/section_17/LINQ/IntroducaoLINQMain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCsharp.section_17.LINQ
+{
+    internal class IntroducaoLINQMain
+    {
+        static void Main(string[] args)
+        {
+            // Specify the data source
+            int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+
+            // Define the query expression
+            var result = numbers.Where((n) => n % 2 == 0).Select(n => n * 10);
+            
+            // Execute the query
+            foreach( int n in result )
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
